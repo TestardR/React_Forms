53ec5ffd92e015625c96ab587178a3cef44cfc64
Romain <rmtestard@gmail.com>
2019-01-03 11:26:04 +0100

    first commit

---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    userName: '',
+    email: '',
+    password: ''
+  };
+  render() {
+    return <div />;
+  }
+}
+
+export default Form;
