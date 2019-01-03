f2e697388c0ef9673ec97a1bcdebf609a3dd1987
Romain <rmtestard@gmail.com>
2019-01-03 11:35:45 +0100

    handling on change for several input

--- src/Form.js
+++ src/Form.js
@@ -4,12 +4,52 @@ class Form extends Component {
   state = {
     firstName: '',
     lastName: '',
-    userName: '',
+    username: '',
     email: '',
     password: ''
   };
+
+  change = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
-    return <div />;
+    return (
+      <form>
+        <input
+          name="firstName"
+          placeholder="First name"
+          value={this.state.firstName}
+          onChange={e => this.change(e)}
+        />
+        <input
+          name="lastName"
+          placeholder="Last name"
+          value={this.state.lastName}
+          onChange={e => this.change(e)}
+        />
+        <input
+          name="username"
+          placeholder="Username"
+          value={this.state.username}
+          onChange={e => this.change(e)}
+        />
+        <input
+          name="email"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={e => this.change(e)}
+        />
+        <input
+          name="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={e => this.change(e)}
+        />
+      </form>
+    );
   }
 }
 
